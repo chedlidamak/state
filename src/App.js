e7a5c6b18a9e1a247ff6ecc4f568096c8a5b1c6c
chedlidamak <chedli_damak@live.com>
2022-08-22 15:55:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -5,16 +5,28 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      show: false,
+      FullName: "Chedli Damak",
+      Bio: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere maiores delectus nisi animi dolorem earum sit et! Ab cumque accusantium ea, harum alias nostrum reiciendis voluptate corrupti, laboriosam debitis ipsa?",
+      Profession: "GoMyCode Bootcamp",
+      show: true,
     };
   }
+
   showData = () => {
     this.setState({ show: !this.state.show });
   };
   render() {
     return (
       <div>
-        {this.state.show ? <MyData /> : "Sorry 🙄"}
+        {this.state.show ? (
+          <MyData
+            name={this.state.FullName}
+            bio={this.state.Bio}
+            Profession={this.state.Profession}
+          />
+        ) : (
+          "Sorry 🙄"
+        )}
         <button onClick={this.showData}>click Me</button>
       </div>
     );
--- src/MyData.js
+++ src/MyData.js
@@ -1,27 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class MyData extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      FullName: "Chedli Damak",
-      Bio: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere maiores delectus nisi animi dolorem earum sit et! Ab cumque accusantium ea, harum alias nostrum reiciendis voluptate corrupti, laboriosam debitis ipsa?",
-      Profession: "GoMyCode Bootcamp",
-      show: false
-    };
+  state = {
+    count: 0,
+  };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState(() => ({
+        count: this.state.count + 1,
+      }));
+    }, 1000);
   }
-  showData=()=>{this.setState({show:!this.state.show})}
+
   render() {
+    const { name, Bio, Profession } = this.props;
     return (
       <div>
-        <h1>Full Name: {this.state.FullName}</h1>
-        <h2>Bio: {this.state.Bio}</h2>
-        <img src="logo192.png" alt="Chedli Damak" />
-        <h4>
-          {this.state.FullName} Profession {this.state.Profession}
-        </h4>
+        <h2> {this.state.count}</h2>
+        <h1>Full Name: {name}</h1>
+        <h2>Bio: {Bio}</h2>
+        <h3>Profession: {Profession}</h3>
       </div>
     );
   }
 }
-export default MyData
+export default MyData;
